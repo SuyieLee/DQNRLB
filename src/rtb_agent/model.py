f2dd3ce3aded87acae417bee7461e31c7accfb00
SuyieLee <bongbong_er@163.com>
2021-04-20 15:45:47 +0800

    步长为5分钟

--- src/rtb_agent/model.py
+++ src/rtb_agent/model.py
@@ -29,12 +29,12 @@ class Network(nn.Module):
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
-        # self.fc3 = nn.Linear(fc2_units, fc3_units)
-        self.fc4 = nn.Linear(fc2_units, action_size)
+        self.fc3 = nn.Linear(fc2_units, fc3_units)
+        self.fc4 = nn.Linear(fc3_units, action_size)
 
     def forward(self, state):
         """Build a network that maps state -> action values."""
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
-        # x = F.relu(self.fc3(x))
+        x = F.relu(self.fc3(x))
         return self.fc4(x)
