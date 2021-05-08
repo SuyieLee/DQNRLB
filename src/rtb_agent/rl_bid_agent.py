3ddad697b50cd846b74825cfb35e6d9792fff245
SuyieLee <bongbong_er@163.com>
2021-05-08 11:18:37 +0800

    完善

--- src/rtb_agent/rl_bid_agent.py
+++ src/rtb_agent/rl_bid_agent.py
@@ -14,7 +14,7 @@ from dqn import Agent
 from reward_net import RewardNet
 import numpy as np
 
-C0 = 1/16
+C0 = 1/2
 Q = 1e5
 anneal = 0.00005
 lamda = 1.0
@@ -279,9 +279,9 @@ class RlBidAgent():
 
 
 def main():
-    global C
-    for i in range(100):
-        print(C)
+    global C0
+    for i in range(5):
+        print(C0)
         # Instantiate the Environment and Agent
         env = gym.make('AuctionEmulator-v0')
         env.seed(0)
@@ -320,7 +320,7 @@ def main():
         print("Total Impressions won with Budget={} Spend={} wins = {} click = {}".format(agent.budget, agent.budget_spend,agent.total_wins,agent.total_rewards))
         print("Total Impressions cmp {} epcp {} value = {}".format(agent.total_spent/agent.total_wins*1000, agent.total_spent/agent.total_rewards, agent.total_rewards))
         env.close()
-        C += 2
+        C0 /= 2
 
 
 if __name__ == "__main__":
